0d0e6bd41e574198e9fea79b8f728c533dc9e851
Ryan Riley Puzon <rrgpuzon@gmail.com>
2024-04-05 14:36:45 -0700

    modifies no-unused-vars rule to not check args

--- server/.eslintrc.cjs
+++ server/.eslintrc.cjs
@@ -2,14 +2,10 @@ module.exports = {
   env: { node: true },
   extends: [
     'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
   rules: {
+    'no-unused-vars': ['warn', {'args': 'none'}]
   },
 }
